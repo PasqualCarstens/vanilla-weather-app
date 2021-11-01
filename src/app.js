70f31aa6bedf376c44f4e2ee61a860f8941fdd45
PasqualCarstens <pasqualcarstens@gmail.com>
2021-11-01 15:31:28 +1100

    Fixed spelling mistake

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "bd7a034b868c09bf6a301a41181ce82e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lat}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
